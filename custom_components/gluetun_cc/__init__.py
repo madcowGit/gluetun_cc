3a3cdebea0b444ec1311c4ad6c9c0c683611516e
madcowGit <yoengsin@hotmail.com>
2026-04-14 16:22:59 +0200

    Update __init__.py with new setup and unload functions

--- custom_components/gluetun_cc/__init__.py
+++ custom_components/gluetun_cc/__init__.py
@@ -1,13 +1,16 @@
-
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault("gluetun", {})
+    hass.data["gluetun"][entry.entry_id] = {
+        "instance_name": entry.data.get("instance_name", "gluetun")
+    }
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data["gluetun"].pop(entry.entry_id, None)
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
